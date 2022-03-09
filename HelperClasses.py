ee9ad331c490f0e5990d4a49b4aac086c73b2496
Nick Alvarez <nickalvarez189@gmail.com>
2022-03-08 22:24:35 -0800

    Shuffle some classes around

--- HelperClasses.py
+++ HelperClasses.py
@@ -73,3 +73,58 @@ class Driver:
     self.driver = webdriver.Chrome(ChromeDriverManager().install(),service_log_path=LOG_PATH, options=chrome_options)
     self.driver.maximize_window()
     self.driver.get(url)
+
+class Stations:
+  def __init__(self):
+    self.stations = dict()
+    self.getAmtrakStations()
+    #self._test_writeToFile()
+  
+  def getAmtrakStations(self):
+    wiki = requests.get("https://en.wikipedia.org/wiki/List_of_Amtrak_stations").text
+    soup = BeautifulSoup(wiki, 'xml')
+    table = soup.find('table', {'class': 'wikitable collapsible sortable'})
+    table_rows = table.find_all('tr')
+
+    for row in table_rows:
+      rowData = row.find_all('td')
+      if len(rowData) > 0:
+        name = rowData[0].text.strip().replace("\u2013","-")
+        code = rowData[1].text.strip()
+        city = rowData[2].text.strip()
+        state = rowData[3].text.strip()
+        self.stations[f"{code} | {name}, {state}"] = {"Code":code, "Name":name, "City":city, "State":state}
+
+  def returnStationKeys(self):
+    return list(self.stations.keys())
+  
+  def returnCityState(self, key):
+    return f"{self.stations[key]['City']}, {self.stations[key]['State']}"
+  
+  def getStationCode(self, key):
+    return self.stations[key]["Code"]
+
+  def _test_writeToFile(self):
+    with open("Stations.json", "w") as f:
+      f.write(json.dumps(self.stations, indent=4))
+
+class UserSelections:
+  def __init__(self):
+    self.origin = None
+    self.destination = None
+    self.departDate = None
+
+    self.userSelections = RailPass()
+  
+  def setOrigin(self, o):
+    self.origin = o
+  def getOrigin(self):
+    return self.origin
+  
+  def setDestination(self, d):
+    self.destination = d
+  def getDestination(self, type):
+    return self.destination
+  
+  def addSegment(self, t):
+    self.userSelections.createSegment(t)
