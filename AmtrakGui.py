ef6af49d452f47fbc0b5c1eff693b22a49fd8922
Nick Alvarez <nickalvarez189@gmail.com>
2022-03-09 16:03:18 -0800

    Threading and image swap

--- AmtrakGui.py
+++ AmtrakGui.py
@@ -15,7 +15,6 @@ if os.name == 'nt':
   SYSTEM_FONT = "Segoe UI"
 elif os.name == 'posix':
   SYSTEM_FONT = "TkDefaultFont"
-#DefaultFont = None
 
 class TitleArea(tk.Frame):
   def __init__(self, parent, *args, **kwargs):
@@ -38,12 +37,21 @@ class ImageArea(tk.Frame):
     self.rightImage = tk.Label(self, image=self.imageCatcher.getCityPhoto(2), width=IMAGE_DIMENSIONS[0], height=IMAGE_DIMENSIONS[1])
     self.rightImage.grid(row=0, column=1, padx=4, pady=16)
 
+    self.leftInfo = tk.Label(self, text=f"{self.leftImage.winfo_width()}x{self.leftImage.winfo_height()}")
+    self.leftInfo.grid(row=1, column=0)
+    self.rightInfo = tk.Label(self, text=f"{self.rightImage.winfo_width()}x{self.rightImage.winfo_height()}")
+    self.rightInfo.grid(row=1, column=2)
+    tk.Button(self, text="Update", command=self._test_widgetDims).grid(row=1, column=1)
+
+  def _test_widgetDims(self):
+    self.leftInfo.config(text=f"{self.leftImage.winfo_width()}x{self.leftImage.winfo_height()}")
+    self.rightInfo.config(text=f"{self.leftImage.winfo_width()}x{self.leftImage.winfo_height()}")
+
   def updateImage(self, side):
     if side == 1:
       self.leftImage.configure(image=self.imageCatcher.getCityPhoto(side))
     elif side == 2:
       self.rightImage.configure(image=self.imageCatcher.getCityPhoto(side))
-
 
 class StationsArea(tk.Frame):
   def __init__(self, parent, *args, **kwargs):
@@ -55,33 +63,48 @@ class StationsArea(tk.Frame):
     self.origin = ttk.Combobox(self, values=self.stations.returnStationKeys())
     self.origin.grid(row=0, column=0, padx=4)
     self.origin.current(random.randint(0, lengthOfList-1))
-    self.origin.bind("<<ComboboxSelected>>", lambda e, widget=self.origin, side=1: self.selectionChangedCallback(widget, side, e))
+    self.origin.bind("<<ComboboxSelected>>", lambda e, widget=self.origin, side=1, isSwap=False: self.selectionChangedCallback(widget, side, isSwap, e))
     self.parent.us.setOrigin(self.stations.getStationCode(self.origin.get()))
 
     self.destination = ttk.Combobox(self, values=self.stations.returnStationKeys())
     self.destination.grid(row=0, column=2, padx=4)
     self.destination.current(random.randint(0, lengthOfList-1))
-    self.destination.bind("<<ComboboxSelected>>", lambda e, widget=self.destination, side=2: self.selectionChangedCallback(widget, side, e))
+    self.destination.bind("<<ComboboxSelected>>", lambda e, widget=self.destination, side=2, isSwap=False: self.selectionChangedCallback(widget, side, isSwap, e))
     self.parent.us.setDestination(self.stations.getStationCode(self.destination.get()))
 
-    self.searchButton = tk.Button(self, text="Find Trains", command=self.startSearch)
-    self.searchButton.grid(row=0, column=1, padx=4)
+    self.swapButton = tk.Button(self, text="<- Swap ->", command=self.swapStations)
+    self.swapButton.grid(row=0, column=1, padx=12)
 
     self.parent.doRefresh(self.stations.returnCityState(self.origin.get()), 1)
     self.parent.doRefresh(self.stations.returnCityState(self.destination.get()), 2)
 
+  def swapStations(self):
+    newOriginCity = self.destination.current()
+    newDestinationCity = self.origin.current()
+    self.origin.current(newOriginCity)
+    self.destination.current(newDestinationCity)
+    self.parent.imageArea.imageCatcher.doCitySwap()
+    self.selectionChangedCallback(self.origin, 1, True)
+    self.selectionChangedCallback(self.destination, 2, True)
+
   def startSearch(self):
     pass
 
-  def selectionChangedCallback(self, widget, side, e=None):
+  def selectionChangedCallback(self, widget, side, isSwap=False, e=None):
+    self.parent.us.set(self.stations.getStationCode(widget.get()), side)
+    lock = self.parent.imageDriverLock
     city = self.stations.returnCityState(widget.get())
-    self.parent.us.setOrigin(self.origin.get())
-    self.parent.us.setDestination(self.destination.get())
-    self.parent.startThread(self.parent.doRefresh, [city, side, self.parent.imageDriverLock])
+    self.parent.startThread(self.parent.doRefresh, [city, side, isSwap, lock])
+
+class TrainResultsArea(tk.Frame):
+  def __init__(self, parent, *args, **kwargs):
+    tk.Frame.__init__(self, parent, *args, **kwargs)
+    
 
 class MainWindow(tk.Tk):
   def __init__(self):
     super().__init__()
+
     self.title("Rail Pass Planner")
     self.iconbitmap("Amtrak_square.ico")
     self.us = UserSelections()
@@ -95,9 +118,13 @@ class MainWindow(tk.Tk):
     self.imageArea.pack()
     self.stationsArea.pack()
 
-  def startThread(self, function, args=[None]):
-    t1 = Thread(target=function, args=args)
-    t1.start()
+    self.update()
+
+  def startThread(self, function, args=None):
+    if args:
+      Thread(target=function, args=args).start()
+    else:
+      Thread(target=function).start()
 
   def onClose(self):
     self.imageArea.imageCatcher.driver.close()
@@ -105,15 +132,20 @@ class MainWindow(tk.Tk):
     self.destroy()
     sys.exit()
 
-  def doRefresh(self, city, side, lock=None):
-    if city != self.imageArea.imageCatcher.getCityName(side):
-      try:
-        with lock:
+  def doRefresh(self, city, side, isSwap=False, lock=None):
+    if isSwap == False:
+      if city != self.imageArea.imageCatcher.getCityName(side):
+        self.imageArea.imageCatcher.setCityPhoto(side, None)
+        self.imageArea.updateImage(side)
+        try:
+          with lock:
+            self.imageArea.imageCatcher.loadImage(city, side, IMAGE_DIMENSIONS)
+        except AttributeError:
           self.imageArea.imageCatcher.loadImage(city, side, IMAGE_DIMENSIONS)
-      except AttributeError:
-        self.imageArea.imageCatcher.loadImage(city, side, IMAGE_DIMENSIONS)
+        self.imageArea.updateImage(side)
+    elif isSwap == True:
       self.imageArea.updateImage(side)
-      self.update_idletasks()
+    self.update_idletasks()
 
 if __name__ == "__main__":
   app = MainWindow()
