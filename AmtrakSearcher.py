b7c33001fc06121d538ca18f434d875c48037279
Nick Alvarez <nickalvarez189@gmail.com>
2022-03-10 11:08:38 -0800

    Logic to find trip type and segments, add pb

--- AmtrakSearcher.py
+++ AmtrakSearcher.py
@@ -25,17 +25,21 @@ class AmtrakSearch:
     self.thisSearchResultsAsTrain = dict()
     self.numberTrainsFound = 0
     self.status = status
+    self.progressbar = None
 
     self.returnedError = False
 
-  def updateStatusMessage(self, message):
+  def updateStatusMessage(self, message, amt=0):
     self.status.set(message)
+    self.progressbar['value'] += amt*2
+    #self.progressbar.step(amt)
     self.root.update_idletasks()
 
-  def preSearchSetup(self, origin, destination, departDate):
+  def preSearchSetup(self, origin, destination, departDate, pb):
     self.origin = origin
     self.destination = destination
     self.departDate = departDate
+    self.progressbar = pb
 
   def _test_returnSearchData(self):
     with open("TestTrainSearch.json", "r") as f:
@@ -75,7 +79,7 @@ class AmtrakSearch:
           if name == "NA":
             name = "Train"
         except:
-          number = 0
+          number = "N/A"
           name = data.find_element(By.XPATH, ".//div[@amt-auto-test-id='search-result-train-name']").text.split("\n")[0]
         depart = data.find_element(By.XPATH, ".//div[@class='departure-inner']")
         departTime = depart.find_element(By.XPATH, ".//div[@class='time mt-2 pt-1 d-flex align-items-baseline']").text.replace("\n","")
@@ -83,7 +87,13 @@ class AmtrakSearch:
         try:
           potentialDelay.find_element(By.XPATH, ".//span[@class='ng-star-inserted']").text # Train is canceled or delayed
         except:
-          travelTime = data.find_element(By.XPATH, ".//div[@class='travel-time d-flex flex-grow-1']").text.split("\n")[1]
+          tripType = data.find_element(By.XPATH, ".//div[@class='travel-time d-flex flex-grow-1']")
+          try:
+            travelTime = tripType.find_element(By.XPATH, ".//span[@class='text-center']").text
+            segmentType = tripType.find_element(By.XPATH, ".//span[@class='segment-display text-center font-semi mt-2 d-block ng-star-inserted']").text
+          except:
+            travelTime = tripType.text.split("\n")[1]
+            segmentType = 1
           arrive = data.find_element(By.XPATH, ".//div[@class='arrival-inner']")
           arrivalTime = arrive.find_element(By.XPATH, ".//div[@class='time mt-2 pt-1 d-flex align-items-baseline']").text.replace("\n", "")
           arrivalDate = None
@@ -99,8 +109,7 @@ class AmtrakSearch:
           sleeperPrice = self.findPrice(prices, 'jl-0-op-0-sleeper')
 
           if not(self.isSoldOut(coachPrice, businessPrice, sleeperPrice)):
-            outputDict = {"Number":number, "Name":name, "Origin":self.origin, "Departure Time":departTime, "Departure Date":self.departDate, "Travel Time":travelTime, "Destination":self.destination, "Arrival Time":arrivalTime, "Arrival Date":arrivalDate, "Coach Price":coachPrice, "Business Price":businessPrice, "Sleeper Price":sleeperPrice}
-            print(outputDict)
+            outputDict = {"Number":number, "Name":name, "Origin":self.origin, "Departure Time":departTime, "Departure Date":self.departDate, "Travel Time":travelTime, "Destination":self.destination, "Arrival Time":arrivalTime, "Arrival Date":arrivalDate, "Coach Price":coachPrice, "Business Price":businessPrice, "Sleeper Price":sleeperPrice, "Segment Type":segmentType}
             if USE_TRAIN_CLASSES:
               self.thisSearchResultsAsTrain.update({self.numberTrainsFound : (Train(outputDict))})
             else:
@@ -113,7 +122,7 @@ class AmtrakSearch:
 
   def checkEveryPage(self, area, pages):
     for page in range(1,pages+1):
-      self.updateStatusMessage(f"Searching - checking page {page}")
+      self.updateStatusMessage(f"Searching - checking page {page}", 22./pages)
       self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
       area.find_element(By.XPATH, f".//*[text()='{page}']").click()
       WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, f".//a[text()='{page}']//ancestor::li[@class='pagination-page page-item active ng-star-inserted']")))
@@ -153,7 +162,7 @@ class AmtrakSearch:
     self.thisSearchResultsAsDict.clear()
     #self.numberTrainsStringVar.set(self.numberTrainsFound)
     try: # Loading the page
-      self.updateStatusMessage("Searching - loading page")
+      self.updateStatusMessage("Searching - loading page", 0)
       if (self.driver.current_url != SEARCH_URL) or self.returnedError:
         self.driver.get(SEARCH_URL)
       self.returnedError = False
@@ -162,32 +171,32 @@ class AmtrakSearch:
       WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//button[@class='am-btn btn--secondary btn--transparent-blue-border']")))
 
       try: # Clicking the new search button
-        self.updateStatusMessage("Searching - opening input fields")
+        self.updateStatusMessage("Searching - opening input fields", 5)
         newSearchButton = self.driver.find_element_by_xpath("//button[@class='am-btn btn--secondary btn--transparent-blue-border']")
         newSearchButton.click()
         time.sleep(1)
 
         try: # Entering to/from stations
-          self.updateStatusMessage("Searching - entering stations")
+          self.updateStatusMessage("Searching - entering stations", 1)
           searchArea = self.driver.find_element_by_xpath("//div[@id='refineSearch']")
           searchArea = searchArea.find_element_by_xpath("//div[@class='row align-items-center']")
           self.enterStationInfo(searchArea)
 
           try: # Entering departure date
-            self.updateStatusMessage("Searching - entering travel dates")
+            self.updateStatusMessage("Searching - entering travel dates", 2)
             self.enterDepartDate(searchArea)
 
             # Wait until "Find Trains" button is enabled, then click it
-            self.updateStatusMessage("Searching - retrieving results")
+            self.updateStatusMessage("Searching - retrieving results", 2)
             WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//button[@class='search-btn ng-star-inserted' and @aria-disabled='false']")))
             searchArea.find_element(By.XPATH, "//div[@class='amtrak-ff-body']").click() # Get calendar popup out of the way
             searchArea.find_element(by=By.XPATH, value="//button[@class='search-btn ng-star-inserted' and @aria-disabled='false']").click() # Click search button
 
             # Search has been completed, but there is no service
             try:
-              self.updateStatusMessage("Searching - loading results")
+              self.updateStatusMessage("Searching - loading results", 2)
               time.sleep(1)
-              self.updateStatusMessage("Searching - checking results")
+              self.updateStatusMessage("Searching - checking results", 10)
               potentialError = self.driver.find_element(By.XPATH, "//div[@class='alert-yellow-text']").text
               print(potentialError)
               self.returnedError = True
@@ -208,30 +217,36 @@ class AmtrakSearch:
               # Search has been completed, and we found a train(s)
               except NoSuchElementException:
                 try:
-                  self.updateStatusMessage("Searching - parsing results page")
+                  self.updateStatusMessage("Searching - parsing results page", 3)
                   WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@class='row ng-tns-c6-1 ng-trigger ng-trigger-searchList ng-star-inserted']")))
                   searchResultsTable = self.driver.find_element(By.XPATH, "//div[@class='row ng-tns-c6-1 ng-trigger ng-trigger-searchList ng-star-inserted']") # Table of results
                   nextPage = searchResultsTable.find_element(By.XPATH, ".//ul[@class='pagination paginator__pagination ng-tns-c6-1']") # Page links area
                   numberSearchPages = int((len(nextPage.find_elements(By.XPATH, ".//*"))-4)/2) # Find out how many pages exist
                   self.checkEveryPage(nextPage, numberSearchPages)
+                  self.updateStatusMessage("Done", 100)
                   
                   #print(json.dumps(self.thisSearchResults, indent=4))
                   return self.thisSearchResultsAsTrain
                 except Exception as e:
                   print("There was an error retrieving the search data.")
                   print(e)
+                  self.returnedError = True
                   return e
           except Exception as e: # Entering departure date
             print("There was an error entering the departure date.")
+            self.returnedError = True
             return e
         except Exception as e: # Entering to/from stations
           print("There was an error entering in station info.")
+          self.returnedError = True
           return e
       except Exception as e: # Clicking the new search button
         print("There was an error clicking the search button.")
+        self.returnedError = True
         return e
     except Exception as e: # Loading the page
       print("There was an issue with loading the search page.")
+      self.returnedError = True
       return e
 
 if __name__ == "__main__":
