82d23bf9fbb73fa2b22dd3ec97b2f3f56746dc29
Nick Alvarez <nickalvarez189@gmail.com>
2022-04-14 18:02:06 -0700

    Add search results navigation for previous entries

--- views/resultsheadingarea.py
+++ views/resultsheadingarea.py
@@ -28,17 +28,91 @@ class ResultsHeadingArea(tk.Frame):
   def __init__(self, parent, *args, **kwargs):
     tk.Frame.__init__(self, parent, *args, **kwargs)
     self.parent = parent
-
-    self.titleToAndFrom = tk.StringVar(self)
-    self.searchDate = tk.StringVar(self)
     self.background = self.parent.resultsBackground
+    self.titleInfoFrame = tk.Frame(self, background=self.background)
+    self.segmentSearchFrame = tk.Frame(self, background=self.background)
+
+    self.searchNum = 1
+    self.searchNumVar = tk.StringVar(self, value=f"Search {self.searchNum}")
+    self.titleToAndFrom = tk.StringVar(self, value="Click \"Find Trains\" to start a search!")
+    self.searchDate = tk.StringVar(self)
+
     self.config(background=self.background)
     self.boldItalic = font.Font(family=cfg.SYSTEM_FONT, size=14, weight=font.BOLD, slant=font.ITALIC)
     self.numberOfTrains = tk.StringVar(self)
 
-    self.titleLabel = tk.Label(self, textvariable=self.titleToAndFrom, font=self.boldItalic, background=self.background)
-    self.titleLabel.pack()#grid(row=0, column=0, pady=4)
-    self.dateLabel = tk.Label(self, textvariable=self.searchDate, font=(cfg.SYSTEM_FONT, 11, font.NORMAL), background=self.background)
+    self.leftSegmentButton = ttk.Button(self.segmentSearchFrame, text='<', state='disabled', command=self.__goLeft)
+    self.rightSegmentButton = ttk.Button(self.segmentSearchFrame, text='>', state='disabled', command=self.__goRight)
+
+    self.leftSegmentButton.pack(side=tk.LEFT, anchor=tk.NW, padx=4)
+    #tk.Label(self.segmentSearchFrame, textvariable=self.searchNum, background=self.background).pack(side=tk.LEFT, anchor=tk.CENTER, fill=tk.X, padx=4, expand=True)
+    self.titleLabel = tk.Label(self.segmentSearchFrame, textvariable=self.titleToAndFrom, font=self.boldItalic, background=self.background)
+    self.titleLabel.pack(side=tk.LEFT, anchor=tk.CENTER, fill=tk.X, padx=4, expand=True)
+    self.rightSegmentButton.pack(side=tk.LEFT, anchor=tk.NE)
+
+    self.dateLabel = tk.Label(self.titleInfoFrame, textvariable=self.searchDate, font=(cfg.SYSTEM_FONT, 11, font.NORMAL), background=self.background)
     self.dateLabel.pack()
-    self.numberLabel = tk.Label(self, textvariable=self.numberOfTrains, background=self.background)
+    self.numberLabel = tk.Label(self.titleInfoFrame, textvariable=self.numberOfTrains, background=self.background)
     self.numberLabel.pack()#grid(row=1, column=0)
+
+    self.segmentSearchFrame.pack(fill=tk.X, anchor=tk.CENTER)
+    self.titleInfoFrame.pack(padx=4)
+  
+  def __updateResultsWidgets(self, doTreeviewRefresh=True):
+    self.searchNumVar.set(f"Search {self.searchNum}")
+    self.__searchButtonToggle()
+
+    # Get updated values
+    thisPreviousSearch = self.parent.us.userSelections.getSearch(self.searchNum)
+    o = thisPreviousSearch["Origin"]
+    d = thisPreviousSearch["Destination"]
+    t = thisPreviousSearch["Date"]
+    r = thisPreviousSearch["Results"]
+
+    # Set updated values
+    self.parent.us.setOrigin(o)
+    self.parent.us.setDestination(d)
+    self.parent.us.setDate(t)
+
+    # Redraw widgets
+    self.titleToAndFrom.set(f"{self.parent.stationsArea.stations.returnStationNameAndState(o)} to {self.parent.stationsArea.stations.returnStationNameAndState(d)}")
+    self.searchDate.set(self.parent.us.getPrettyDate())
+    if doTreeviewRefresh: self.parent.trainResultsArea.refreshHandler(r)
+
+  def changeSearchView(self, searchnum):
+    if (searchnum <= self.parent.us.userSelections.numSearches) and (searchnum >= 1):
+      self.searchNum = searchnum
+    else:
+      self.searchNum = self.parent.us.userSelections.numSearches
+    self.__updateResultsWidgets((False if searchnum == -1 else True))
+
+  def __goLeft(self):
+    # Populate a treeview
+    if self.searchNum > 1:
+      self.searchNum -= 1
+      self.__updateResultsWidgets()
+  
+  def __goRight(self):
+    # Populate a treeview
+    if self.searchNum < self.parent.us.userSelections.numSearches:
+      self.searchNum += 1
+      self.__updateResultsWidgets()
+
+  def __searchButtonToggle(self):
+    currentView = self.searchNum
+    totalSearches = self.parent.us.userSelections.numSearches
+
+    if currentView == 1:
+      self.leftSegmentButton.config(state='disabled')
+      if totalSearches == 1:
+        self.rightSegmentButton.config(state='disabled')
+      elif totalSearches > 1:
+        self.rightSegmentButton.config(state='normal')
+    elif currentView > 1:
+      self.leftSegmentButton.config(state='normal')
+      if totalSearches > currentView:
+        self.rightSegmentButton.config(state='normal')
+      elif totalSearches == currentView:
+        self.rightSegmentButton.config(state='disabled')
+    
+    self.update_idletasks()
