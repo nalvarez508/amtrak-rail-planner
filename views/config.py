ecb0bf9cec5850739d9276fa66628772972f8e19
Nick Alvarez <48071045+nicky189@users.noreply.github.com>
2022-04-21 16:21:33 -0700

    Increase width for datetime column

--- views/config.py
+++ views/config.py
@@ -5,13 +5,13 @@ SEARCH_URL = "https://www.amtrak.com/tickets/departure.html"
 IMAGE_DIMENSIONS = [300,225]
 if os.name == 'nt':
   SYSTEM_FONT = "Segoe UI"
-  GEOMETRY = "650x875+50+50"
+  GEOMETRY = "700x875+50+50"
   MINSIZE = [635, 730]
   BACKGROUND = "SystemButtonFace"
   WIDTH_DIV = 1
 elif os.name == 'posix':
   SYSTEM_FONT = "TkDefaultFont"
-  GEOMETRY = "700x680+0+0"
+  GEOMETRY = "750x680+0+0"
   MINSIZE = [700, 680]
   BACKGROUND = "gray93"
   WIDTH_DIV = 1.5
