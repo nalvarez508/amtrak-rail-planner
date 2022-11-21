19a9861329c699d5ae747f05ff454a1842243b2b
Nick Alvarez <nickalvarez189@gmail.com>
2022-11-21 15:36:24 -0800

    Change window width for search icons

--- views/config.py
+++ views/config.py
@@ -8,14 +8,14 @@ DEV_MODE = True
 if os.name == 'nt':
   SYSTEM_FONT = "Segoe UI"
   GEOMETRY = "700x875+50+50"
-  MINSIZE = [650, 730]
+  MINSIZE = [745, 730]
   BACKGROUND = "SystemButtonFace"
   WIDTH_DIV = 1
   ICON = "tracks.ico"
 elif os.name == 'posix':
   SYSTEM_FONT = "TkDefaultFont"
   GEOMETRY = "750x680+0+0"
-  MINSIZE = [700, 680]
+  MINSIZE = [795, 680]
   BACKGROUND = "gray93"
   WIDTH_DIV = 1.5
   ICON = "tracks.icns"
